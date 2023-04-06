33a8d3ed47f95f32d55edf685195a193259ba54d
Daniel-gmol <a01285254@tec.mx>
2023-04-06 10:56:17 -0600

    grafico3DCalculoCE
    Nueva función para graficar en 3D las los vectores de campo eléctrico. Además se agregó al archivo principal los ajustes para graficar las partículas en 3D. Igualmente el calculo de campoElectrico se le agrego la componente Z para tomar en cuenta en los procedimientos en tres dimensiones.

--- campoElectrico.m
+++ campoElectrico.m
@@ -1,5 +1,5 @@
 % Cálculo vectorial de campo eléctrico por partícula
-function [campoElectricoX,campoElectricoY] = campoElectrico(vCoordenadas, vCargas, particulaCampo, n)
+function [campoElectricoX,campoElectricoY, campoElectricoZ] = campoElectrico(vCoordenadas, vCargas, particulaCampo, n)
 
 % Constante k
 k = 8.99 * 10^9; % Nm^2/C^2
@@ -7,11 +7,13 @@ k = 8.99 * 10^9; % Nm^2/C^2
 % Creación de vectores para almacenar calculo del campo en cada componente
 campoElectricoX = 1:n; % Campo eléctrico componente X
 campoElectricoY = 1:n; % Campo eléctrico componente Y
+campoElectricoZ = 1:n; % Campo eléctrico componente Z
 
 for i = 1:n
     % Distancia: √(xp - xi)^2 + (yp - yi)^2
     distancia = ((vCoordenadas(particulaCampo,1) - vCoordenadas(i,1))^2 ...
-    + (vCoordenadas(particulaCampo,2) - vCoordenadas(i,2))^2)^(1/2); 
+              + (vCoordenadas(particulaCampo,2)  - vCoordenadas(i,2))^2 ...
+              + (vCoordenadas(particulaCampo,3)  - vCoordenadas(i,3))^2)^(1/2); 
 
     if distancia ~= 0
         % Vector Unitario r;
@@ -20,7 +22,10 @@ for i = 1:n
 
         componenteY = (vCoordenadas(i,2) ...
                        - vCoordenadas(particulaCampo,2)) / distancia;
-                   
+        
+        componenteZ = (vCoordenadas(i,3) ...
+                       - vCoordenadas(particulaCampo,3)) / distancia;
+  
         % E = k * q / r^2 * vector unitario
         % Si las cargas son iguales el campo es inverso al vector unitario
         if vCargas(i) > 0 && vCargas(particulaCampo) > 0 ...
@@ -31,6 +36,9 @@ for i = 1:n
 
             campoElectricoY(i) =  - k * abs(vCargas(i)) ...
                                     / distancia^2 * componenteY;
+                                
+            campoElectricoZ(i) =  - k * abs(vCargas(i)) ...
+                                    / distancia^2 * componenteZ;
         % Si son diferentes, campo va en misma direcicón al vector unitario    
         else
             campoElectricoX(i) =  k * abs(vCargas(i))...
@@ -38,11 +46,15 @@ for i = 1:n
 
             campoElectricoY(i) =   k * abs(vCargas(i)) ...
                                        / distancia^2 * componenteY;
+                                   
+            campoElectricoZ(i) =   k * abs(vCargas(i)) ...
+                                       / distancia^2 * componenteZ;
         end
         
     % Si la distancia es 0, campoElectrico = indefnidio = 0
     else
         campoElectricoX(i) =  0;
-        campoElectricoY(i) = 0;               
+        campoElectricoY(i) = 0;
+        campoElectricoZ(i) = 0;
     end
 end
--- electrodos.m
+++ electrodos.m
@@ -11,7 +11,7 @@
     
 %}
 
-%% Campo electrico partículas simples
+%% Campo electrico partículas simples 2D
 
 clc
 clear
@@ -54,13 +54,13 @@ n = length(vCoordenadas);
 
 % Prueba compleja
 % Agregrar unidades
-vCoordenadas = [[2,1;
-                 2,2;
-                 2,3];
+vCoordenadas = [[2,1,0;
+                 2,2,0;
+                 2,3,0];
                
-               [4,1;
-                4,2;
-                4,3;]];
+               [4,1,0;
+                4,2,0;
+                4,3,0;]];
             
 % Agregrar unidades
 vCargas = [1.6022e-9;1.6022e-9;1.6022e-9;
@@ -100,6 +100,7 @@ else
     particulaCampo = input("Particula a calcular campo E: ");
 end
 
+
         
 % Plot de las coordenadas de las partículas en 2D según la carga
 for i = 1:n
@@ -117,8 +118,9 @@ axis equal  % Limita ejes del plot
 % PRUEBA DE EJES: axis([-5 12 -5 12])
 
 
+
 % Función campo Eléctrico
-[campoElectricoX,campoElectricoY] = campoElectrico(vCoordenadas, vCargas, particulaCampo, n);
+[campoElectricoX,campoElectricoY, ~] = campoElectrico(vCoordenadas, vCargas, particulaCampo, n);
 
 
 % Graficación vectores campo eléctrico en la partícula
@@ -132,3 +134,98 @@ graficoVectores(xp, yp, campoElectricoX, campoElectricoY);
 % Calculo magnitud de campo eléctrico
 magnitudCampoE = ((sum(campoElectricoX))^2 + ...
                   (sum(campoElectricoY))^2)^(1/2);
+disp("Magnitud campo eléctrico en (" + ...
+    vCoordenadas(particulaCampo,1) + ", " + vCoordenadas(particulaCampo,2) + "): " ...
+    + magnitudCampoE) % Agregar unidades?
+
+%% Campo electrico partículas simples 3D
+
+clc
+
+% Prueba compleja
+% Agregrar unidades
+vCoordenadas = [[2,2,1;
+                 2,2,2;
+                 2,2,3];
+               
+               [4,4,1;
+                4,4,2;
+                4,4,3;]];
+            
+% Agregrar unidades
+vCargas = [1.6022e-9;1.6022e-9;1.6022e-9;
+           -1.6022e-9;-1.6022e-9;-1.6022e-9;];
+
+% Cantidad de partículas        
+n = length(vCoordenadas);
+
+
+% Partícula en donde se halla/cualcula el campo eléctrico
+
+% Preguntar si se Calcula para Particula de Prueba o Particula Existente
+particulaPrueba = input("Particula existente (E) o de prueba (P): ", "s");
+
+if particulaPrueba == "P"
+    
+    % Coordenada partícula de prueba en donde se calcula Campo Eléctrico
+    coordenadaCampoX = input("Coordenada X: ");
+    coordenadaCampoY = input("Coordenada Y: ");
+    coordenadaCampoZ = input("Coordenada Z: ");
+    
+    % Almacenamiento de coordenadas de particula prueba
+    % en arreglo de coordenadas de partpículas existentes
+    vCoordenadas(end + 1, 1) = coordenadaCampoX;
+    vCoordenadas(end, 2) = coordenadaCampoY;
+    vCoordenadas(end, 3) = coordenadaCampoZ;
+    
+    % Añadir carga de prueba a vector de cargas
+    vCargas(end + 1, 1) = 1;
+    
+    % Aumentar número de partículas/ se agrega la de prueba
+    n = n + 1;
+    
+    % Partícula en que se evalua el campo es la última en el arreglo
+    % Posición n
+    particulaCampo = n;
+else
+    % Particula en dodne se calculará el campo eléctrico
+    particulaCampo = input("Particula a calcular campo E: ");
+end
+
+
+        
+% Plot de las coordenadas de las partículas en 3D según la carga
+for i = 1:n
+   if vCargas(i) < 0
+       % Azul cargas negativas
+       plot3(vCoordenadas(i), vCoordenadas(i, 2), vCoordenadas(i, 3), '.b')
+       hold on
+   else
+       % Rojo cargas positivas
+       plot3(vCoordenadas(i), vCoordenadas(i, 2), vCoordenadas(i, 3), '.r')
+       hold on
+   end
+end
+% axis equal   Limita ejes del plot
+% PRUEBA DE EJES: axis([-5 12 -5 12])
+
+
+
+% Función campo Eléctrico
+[campoElectricoX,campoElectricoY, campoElectricoZ] = campoElectrico(vCoordenadas, vCargas, particulaCampo, n);
+
+
+% Graficación vectores campo eléctrico en la partícula
+
+% Creación de matriz repitiendo coordenada X - Y en donde inicia el vector
+xp = repmat(vCoordenadas(particulaCampo,1), 1, n);
+yp = repmat(vCoordenadas(particulaCampo,2), 1, n);
+zp = repmat(vCoordenadas(particulaCampo,3), 1, n);
+
+graficoVectores3(xp, yp, zp, campoElectricoX, campoElectricoY, campoElectricoZ);
+
+
+% Calculo magnitud de campo eléctrico
+magnitudCampoE = ((sum(campoElectricoX))^2  + ...
+                  (sum(campoElectricoY))^2  + ...
+                  (sum(campoElectricoZ))^2) ^ (1/2);
